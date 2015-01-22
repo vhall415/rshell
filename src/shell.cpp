c7653599075418184fafe79f922067e81ec4203c
Victoria Hall <vhall001@hammer.cs.ucr.edu>
2015-01-21 17:40:01 -0800

    updated shell.cpp

--- src/shell.cpp
+++ src/shell.cpp
@@ -7,7 +7,30 @@ int main()
 	char *cmd = "";
 	while(cmd != "exit")
 	{
-
+		char *line;
+		cout << "$";	//print command prompt
+		getline(cin, line);	//puts the user text in line
+		int i = 0;	//index for line
+		if(line[i] == " " || line[i] == "\t")	//line starts with space/tab
+		{
+		while(line[i] == " " || line[i] == "\t")	//loop until not a space or tab
+		i++;
+		}
+		if(line[i] == "#")
+			break;
+		while(line[i] != " " || line[i] != "\t" || line[i] != "&&" || line[i] != ";" || line[i] != "||")
+		{
+			cmd += line[i];
+			i++;
+		}
+		/*
+		while(i < line.size())	
+		{
+		
+		}
+		*/
+		cout << cmd << endl;
+		cmd = "exit";
 	}
 	return 0;
 }
